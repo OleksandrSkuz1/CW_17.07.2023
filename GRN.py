8f4179913830dc6a0cce12f4cbac2462130a7ad4
OleksandrSkuz1 <138138803+OleksandrSkuz1@users.noreply.github.com>
2023-07-17 19:40:50 +0300

    add

--- GRN.py
+++ GRN.py
@@ -5,12 +5,27 @@
 # a = float(input('Введіть кількість доларів: '))
 # GRN_USD(a)
 
-def factorial(fac):
-    num = 1
-    while fac > 1:
-        num *= fac
-        fac -= 1
-    print(num)
+# def factorial(fac):
+#     num = 1
+#     while fac > 1:
+#         num *= fac
+#         fac -= 1
+#     print(num)
+#
+# a = int(input("n! = "))
+# factorial(a)
 
-a = int(input("n! = "))
-factorial(a)
+def isPolindrom(word):
+    reverse_word = word[::-1]
+    print(reverse_word)
+    if reverse_word == word:
+        return True
+    else:
+        return False
+
+if __name__ == "__main__":
+    value = input("Введіть слово для перевірки(поліндром): ")
+    print(isPolindrom(value))
+
+
+
