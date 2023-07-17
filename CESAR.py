f7ab9c1beed09656704cc8a73c964fb9586e2e80
OleksandrSkuz1 <138138803+OleksandrSkuz1@users.noreply.github.com>
2023-07-17 20:04:15 +0300

    add

--- CESAR.py
+++ CESAR.py
@@ -1,7 +1,8 @@
-if __name__ == "__main__":
-    enter_txt = 'Python'
+
+
 def cesarCode(txt, key):
     witn open("alfabet.json", 'r') as file:
+
      alfabet_data = json.load(fale)
     code_in_number = []
     for letter in txt:
